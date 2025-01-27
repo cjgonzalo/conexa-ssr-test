29268b6fe32e740031491e8bb67f43bf3244f1fc
Gonzalo Casanova <gjcasanova@teco.com.ar>
2025-01-27 01:16:19 -0300

    new imports added

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from '../users/users.module';
+import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from '../auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot("")],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get("DATABASE_URI")
+      })
+    }),
+    UsersModule,
+    RolesModule,
+    AuthModule
+  ]
 })
+
 export class AppModule {}
