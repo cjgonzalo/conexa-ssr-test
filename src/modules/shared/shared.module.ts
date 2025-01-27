0fb9b9bd79d8a0a00458eb0b350a9c3cba22f5b4
Gonzalo Casanova <gjcasanova@teco.com.ar>
2025-01-27 20:40:28 -0300

    added new imports and providers to shared module

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ResponseService } from './responses.service';
+import { DynamicModelService } from './dynamic-model.service';
+import { SwapiModule } from '../swapi/swapi.module';
+import { SwapiService } from '../swapi/swapi.service';
 
 @Module({
-  providers: [ResponseService],
-  exports: [ResponseService]
+  imports: [SwapiModule],
+  providers: [ResponseService, DynamicModelService, SwapiService],
+  exports: [ResponseService, DynamicModelService]
 })
 
 export class SharedModule {}
