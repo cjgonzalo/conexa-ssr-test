5f67da8566989e695a85fe2bad24306e846f1d0b
Gonzalo Casanova <gjcasanova@teco.com.ar>
2025-01-27 20:39:35 -0300

    new responses added to response service

--- src/modules/shared/responses.service.ts
+++ src/modules/shared/responses.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ResponseService {
-  constructor() {}
 
-  success(message: string, data: any = null) {
-    return { success: true, message, data }
+  success(message: string, httpCode = HttpStatus.OK, data: any = null) {
+    return { success: true, message, httpCode, data }
   }
   
-  error(message: string, httpCode: number = 400) {
+  error(message: string, httpCode: number = HttpStatus.BAD_REQUEST) {
     return { success: false, message, httpCode }
+  }
+
+  create(message: string, httpCode: number = HttpStatus.CREATED, data: any) {
+    return { success: true, message, httpCode, data }
+  }
+
+  update(message: string, httpCode: number = HttpStatus.OK, data: any) {
+    return { success: true, message, httpCode, data }
+  }
+
+  delete(message: string, httpCode: number = HttpStatus.OK, data: any) {
+    return { success: true, message, httpCode, data }
   }
 }
