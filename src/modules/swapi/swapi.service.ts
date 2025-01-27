63892a6882982a88600072ea5ba5279609c65820
Gonzalo Casanova <gjcasanova@teco.com.ar>
2025-01-27 20:41:17 -0300

    new logic added to swapi service

--- src/modules/swapi/swapi.service.ts
+++ src/modules/swapi/swapi.service.ts
@@ -1,25 +1,27 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { populateDatabase } from './db/swapi-db';
+import fetch from 'node-fetch';
+import SwapiInterface from '../shared/interfaces/swapi.interface';
 
 @Injectable()
-export class SwapiService implements OnModuleInit{
-  constructor(private readonly configService: ConfigService) {}
+export class SwapiService{
+  constructor(
+    private readonly configService: ConfigService,
+  ) {}
 
-  private async getResources(): Promise<Object> {
+  async getResources(): Promise<string[]> {
     const swapiBaseUrl = this.configService.get("SWAPI_BASE_URL")
 
     if(!swapiBaseUrl) {
       throw new Error("Swapi url not defined")
     }
     const resources = await fetch(swapiBaseUrl)
-    return await resources.json()
+    const urls = await resources.json() as Object
+    return Object.values(urls)
   }
 
-  async onModuleInit() {
-    const resources = await this.getResources()
-    for(const resource of Object.values(resources)) {
-      await populateDatabase(resource)
-    }
+  async getSwapiData(resource: string): Promise<SwapiInterface> {
+    const response = await fetch(resource)
+    return await response.json() as SwapiInterface
   }
 }
